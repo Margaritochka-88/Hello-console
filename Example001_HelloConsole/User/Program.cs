029aa8d15a0635e2b87368dd7b707f8424a0e820
Margaritochka-88 <113050315+Margaritochka-88@users.noreply.github.com>
2022-09-18 23:09:44 +0300

    added username

---
+++ Example001_HelloConsole/User/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "марго")
+{
+    Console.WriteLine("Ура, это же Марго!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
